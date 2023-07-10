25495cf84c7cf34f27de89ae942cc2fabed80dda
shourya <shourya.agarwal@itential.com>
2023-07-10 15:35:42 -0400

    Integrated with SDK

--- src/action.js
+++ src/action.js
@@ -1,7 +1,26 @@
-const { getInput, setOutput, setFailed } = require("@actions/core");
-const { get, post } = require('axios')
+//const { getInput, setOutput, setFailed } = require("@actions/core");
+import { getInput, setOutput, setFailed } from "@actions/core";
+//const { get, post } = require('axios')
+import axios from 'axios'
+import { ItentialSDK } from "ea-utils/sdk.js";
+
 
 async function run() {
+
+   //test variables
+  /*const iap_token = 'MzRhNTFhMDczM2Q5OTVmNzk5ZGVlYTBjZGQxN2MyODY=';
+  const api_endpoint_body = {};
+  const time_interval = 15;
+  const no_of_attempts = 10;
+  const automation_status = 1;
+  let iap_instance = 'https://itential-se-poc-stg-221.trial.itential.io/';
+  if (iap_instance.endsWith('/'))
+    iap_instance = iap_instance.substring(0, iap_instance.length - 1);
+  let api_endpoint = 'Scott';
+  if (api_endpoint.startsWith('/'))
+    api_endpoint = api_endpoint.substring(1);
+  */
+ 
   const iap_token = getInput("iap_token");
   const api_endpoint_body = JSON.parse(getInput("api_endpoint_body"));
   const time_interval = getInput("time_interval");
@@ -13,12 +32,28 @@ async function run() {
   let api_endpoint = getInput("api_endpoint");
   if (api_endpoint.startsWith('/'))
     api_endpoint = api_endpoint.substring(1);
+  
   let count = 0;
 
+   //using the ea-utils library
+   const user = [
+    {
+      hostname: iap_instance,
+      username: '',
+      password: '',
+      token: iap_token
+    }
+  ]
+
+  const authentication = new ItentialSDK.Authentication(user); 
+  const opsManager = new ItentialSDK.OperationsManagerAPI(authentication.users[0].hostname, authentication.users[0].userKey, authentication);
+  const health = new ItentialSDK.HealthAPI(authentication.users[0].hostname, authentication.users[0].userKey, authentication);
+  
   try {
     //check the status of the automation and return the output (IAP release <= 2021.1)
     const automationStatus211 = (automation_id) => {
-      get(
+      axios
+      .get(
         `${iap_instance}/workflow_engine/job/${automation_id}/details?token=` +
         iap_token
       )
@@ -30,7 +65,8 @@ async function run() {
               automationStatus211(automation_id);
             }, time_interval * 1000);
           } else if (res.data.status === "complete") {
-            get(
+            axios
+            .get(
               `${iap_instance}/workflow_engine/job/${automation_id}/output?token=` +
               iap_token
             )
@@ -57,70 +93,81 @@ async function run() {
 
     //check the status of the automation and return the output (IAP release > 2021.1)
     const automationStatus221 = (automation_id) => {
-      get(
-        `${iap_instance}/operations-manager/jobs/${automation_id}?token=` + iap_token
-      )
-        .then((res) => {
-          console.log("Automation Status: ", res.data.data.status);
-          if (res.data.data.status === "running" && count < no_of_attempts) {
-            console.log("Count: ", count);
+
+      console.log("sdk integrated");
+
+      opsManager.getJobResult(automation_id, (res,err) => {
+        if (err){
+          if (typeof err === "string") {
+            setFailed(err);
+          } else {
+            setFailed(err.response.data);
+          }
+        } else {
+          console.log("Automation Status: ", res.status);
+          if (res.status === "running" && count < no_of_attempts) {
+            console.log("count: ", count);
             setTimeout(() => {
               count += 1;
               automationStatus221(automation_id);
             }, time_interval * 1000);
-          } else if (res.data.data.status === "complete") {
-            setOutput("results", res.data.data.variables);
-          } else if (res.data.data.status === "canceled") {
+          } else if (res.status === "complete") {
+            setOutput("results", res.variables);
+          } else if (res.status === "canceled") {
             setFailed("Automation Canceled");
-          } else if (res.data.data.status === "error") {
-            setFailed(res.data.data.error);
+          } else if (res.status === "error") {
+            setFailed(res.error);
           } else {
             setFailed(
-              "Automation Timed out based upon user defined time_interval and no_of_attempts"
+              'Automation Timed out based upon user defined time_interval and no_of_attempts'
             );
           }
-        })
-        .catch((err) => {
-          setFailed(err.response.data);
-        });
+
+        }
+      })
     };
 
     //start the automation on GitHub event
     const startAutomation = () => {
-      get(`${iap_instance}/health/server?token=` + iap_token)
-        .then((res) => {
-          const release = res.data.release.substring(
+
+      health.getServerHealth((res, err)=> {
+
+        console.log("checked the health");
+
+        if(err){
+          setFailed(err.response.data);
+        } else {
+
+          const release = res.release.substring(
             0,
-            res.data.release.lastIndexOf(".")
+            res.release.lastIndexOf(".")
           );
 
-          post(
-            `${iap_instance}/operations-manager/triggers/endpoint/${api_endpoint}?token=` +
-            iap_token,
-            api_endpoint_body
-          )
-            .then((res) => {
+          opsManager.startJobEndpoint(api_endpoint, api_endpoint_body, (res, err)=> {
+
+            console.log("triggered endpoint using sdk");
+
+            if(err){
+              setFailed(err.response.data);
+            }else {
               if (Boolean(Number(automation_status)) === true) {
-                if (Number(release) <= 2021.1) automationStatus211(res.data._id);
-                else automationStatus221(res.data.data._id);
+                if (Number(release) <= 2021.1) automationStatus211(res._id);
+                else automationStatus221(res.data._id);
               }
               else {
-                if (Number(release) <= 2021.1) setOutput("results", res.data._id);
-                else setOutput("results", res.data.data._id);
+                if (Number(release) <= 2021.1) setOutput("results", res._id);
+                else setOutput("results", res.data._id);
               }
-            })
-            .catch((err) => {
-              setFailed(err.response.data);
-            });
-        })
-        .catch((err) => {
-          setFailed(err);
-        });
+            }
+          });
+        }
+      });
     };
 
     startAutomation();
-  } catch (e) {
-    setFailed(e);
+
+  } catch (err) {
+    setFailed(err);
   }
 }
 
